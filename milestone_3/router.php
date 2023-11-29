2f2119a9ba2990dd4268f25a688605539402788c
Pillairenu <grenupillai91@gmail.com>
2023-11-28 23:54:12 -0800

    Refactor code to use the variable for dynamic path handling

--- milestone_3/router.php
+++ milestone_3/router.php
@@ -1,4 +1,8 @@
 <?php
+
+// Include the configuration file
+include('config.php');
+
 //$uri = str_replace('/DGL_123/milestone_3/', '', $_SERVER['REQUEST_URI']);
 $uri =parse_url($_SERVER['REQUEST_URI'])['path'];
 
@@ -6,17 +10,17 @@ $uri =parse_url($_SERVER['REQUEST_URI'])['path'];
 
 $routes=[
 
-    '/DGL_123/milestone_3/'=>'controllers/index.php',
-    '/DGL_123/milestone_3/signin'=>'controllers/signin.php',
-    '/DGL_123/milestone_3/signup'=>'controllers/signup.php',
-    '/DGL_123/milestone_3/rooms'=>'controllers/rooms.php',
-    '/DGL_123/milestone_3/house'=>'controllers/house.php',
-    '/DGL_123/milestone_3/apartment'=>'controllers/apartment.php',
-    '/DGL_123/milestone_3/room-details'=>'controllers/room-details.php',
-    '/DGL_123/milestone_3/favorite'=>'controllers/favorite.php',
-    '/DGL_123/milestone_3/property'=>'controllers/add_property.php',
-    '/DGL_123/milestone_3/bookings'=>'controllers/view_bookings.php',
-    '/DGL_123/milestone_3/logout'=>'controllers/logout.php'
+    $proj_root.'/'=>'controllers/index.php',
+    $proj_root.'/signin'=>'controllers/signin.php',
+    $proj_root.'/signup'=>'controllers/signup.php',
+    $proj_root.'/rooms'=>'controllers/rooms.php',
+    $proj_root.'/house'=>'controllers/house.php',
+    $proj_root.'/apartment'=>'controllers/apartment.php',
+    $proj_root.'/room-details'=>'controllers/room-details.php',
+    $proj_root.'/favorite'=>'controllers/favorite.php',
+    $proj_root.'/property'=>'controllers/add_property.php',
+    $proj_root.'/bookings'=>'controllers/view_bookings.php',
+    $proj_root.'/logout'=>'controllers/logout.php'
 
 
 ];
